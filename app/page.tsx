30b5a5dc2e57476b197da65d1dcff6895f59903f
iamphelipe <phelipeguimaraes2023@gmail.com>
2026-01-21 12:14:21 -0300

    feat: setup do projeto e criando os componentes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator";
+import { ArrowDownRight, Check, List, Plus } from "lucide-react";
+
 const Home = () => {
   return (
-    <div>
-      Odair
-    </div>
-  )
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg p-4">
+        <div className="flex gap-2">
+          <Input placeholder="Adicionar tarefa"/>
+          <Button variant="outline" className="cursor-pointer"> <Plus /> Cadastrar</Button>
+        </div>
+        <Separator />
+        <div className="flex gap-2">
+          <Badge className="cursor-pointer"><List/>Todas</Badge>
+          <Badge className="cursor-pointer"><ArrowDownRight/>Não finalizadas</Badge>
+          <Badge className="cursor-pointer"><Check/>Concluídos</Badge>
+        </div>
+      </Card>
+    </main>
+  );
 }
 
 export default Home
